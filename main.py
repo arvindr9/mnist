164b39d3b42a1dbe2ce02e217b204e4e30de5c93
Arvind Ramaswami <saramaswami@gmail.com>
2017-10-16 13:21:31 -0300

    added more files including muself working through the Tensorflow tutorial

--- main.py
+++ main.py
@@ -1,2 +1,30 @@
 import numpy as np
 import pandas as pd
+import jupyter
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+
+
+
+import scipy
+import tensorflow as tf
+
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+import tensorflow as tf
+
+learning_rate = 1e-4
+
+num_iterations = 2500
+
+dropout = 0.5
+batch_size = 50
+
+validation_size = 2000
+image_to_display = 10
+
+data = pd.read_csv('.../input/train.csv')
+print('data({0[0]}, {0[1]})'.format(data.shape))
+print(data.head())
---
+++ settings.py
@@ -0,0 +1,9 @@
+learning_rate = 1e-4
+
+num_iterations = 2500
+
+dropout = 0.5
+batch_size = 50
+
+validation_size = 2000
+image_to_display = 10
